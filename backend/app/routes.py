b351809f8c062335ada61553a39942be99d99d6f
Santiago Miro <mirot.santiago@gmail.com>
2024-09-15 15:37:31 -0400

    Add text extraction to pipeline

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,6 +1,10 @@
 import os
+import sys
 from flask import Blueprint, request, jsonify
 from werkzeug.utils import secure_filename
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), './services/')))
+from services.extract_text import extract_text
 
 # Create a Blueprint for routing
 upload_routes = Blueprint('upload_routes',__name__)
@@ -25,8 +29,13 @@ def upload_file():
     
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
-        file.save(os.path.join(UPLOAD_FOLDER, filename))
-        return jsonify({'message':f'File {filename} uploaded successfully.'}), 200
+        file_path = os.path.join(UPLOAD_FOLDER, filename)
+        file.save(file_path)
+
+        # Extract text from file
+        extracted_text = extract_text(file_path)
+        print(extracted_text)
+        return jsonify({'message': f'File {filename} uploaded successfully.', 'text': extracted_text}), 200
     
     return jsonify({'error':'File type not allowed'}), 400
 
